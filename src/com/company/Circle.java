a68bb59fa932ff9317228f2866b4c58acf2a9383
profits <v-profit@yandex.ru>
2021-08-18 15:22:34 +0400

    1. задание 1 (Circle)

---
+++ src/com/company/Circle.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Circle {
+    public static final double RADIUS = 1;
+    public static final String COLOR = "красный";
+
+    private double radius;
+    private String color;
+
+    public Circle() { //1-й конструктор
+        radius = RADIUS;
+        color = COLOR;
+    }
+    public Circle(double r) { // 2-й конструктор
+        radius = r;
+        color = COLOR;
+    }
+    public Circle(double r, String c) { // 3-й конструктор
+        radius = r;
+        color = c;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public String getColor() {
+        return color;
+    }
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public String toString(){
+        return "Paдиyc = " + this.radius + ", Цвет - " + this.color +
+                ", Площадь = " + this.getArea();
+    }
+    //коммент для проверки коммита
+}
