a41c92c6df23e8fcedf965b1c2f1ad0145962522
profits <v-profit@yandex.ru>
2021-08-18 12:23:56 +0400

    1. создание класса Time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,24 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Circle c1 = new Circle();
-        System.out.println(c1.toString()); // явный вызов метода toString()
-        System.out.println(c1); //неявное обращение к cl.toString()
-        System.out.println("Объект c1 это: " + c1); //'+' вызывает cl.toString(), чтобы получить строку до конкатенации
+        Time t1 = new Time();
+        System.out.println(t1);
 
-        Circle c2 = new Circle(2);
-        System.out.println(c2);
+        Time t2 = new Time(1,2,3);
+        for(int i = 0; i < 3; ++i)
+            System.out.println(t2.nextSecond());
+
+        t2.setHour(4);
+        t2.setMinute(5);
+        t2.setSecond(6);
+        System.out.println(t2);
+        System.out.println("Hour: " + t2.getHour());
+        System.out.println("Minute: " + t2.getMinute());
+        System.out.println("Secund: " + t2.getSecond());
+
+        t2.setTime(23,59,58);
+        System.out.println(t2);
+        System.out.println(t2.nextSecond());
+        System.out.println(t2.nextSecond().nextSecond().nextSecond());
     }
 }
