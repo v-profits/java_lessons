46ceaed5920621ef0a0650f8537a832469c42c9f
profits <v-profit@yandex.ru>
2021-08-18 15:06:24 +0400

    1. объновление Main (Account)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,21 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Circle с1 = new Circle();
-        System.out.println(с1.toString()); // явный вызов метода toString()
-        System.out.println(с1); //неявное обращение к с1.toString()
-        System.out.println("Объект c1 это: " + с1); //'+' вызывает с1.toString(), чтобы получить строку до конкатенации
 
+        Account a1 = new Account(1234, 99.99);
+        System.out.println(a1); // toStringO
+        Account a2 = new Account(8888);
+        System.out.println(a2); // проверка toStringO
+
+        a1.setBalance(88.88);
+        System.out.println(a1); // обращаемся к toStringO Для проверки измененного объекта
+        System.out.println("Hoмep счета: " + a1 .getAccount());
+        System.out.println("Баланc: " + a1 .getBalance());
+        a1.credit(10);
+        System.out.println(a1); // вызываем toString()/pni проверки измененного объекта
+        a1 .debit(5);
+        System.out.println(a1);
+        a1 .debit(500); // Проверка метода debit() при наличии ошибки
+        System.out.println(a1);
     }
 }
