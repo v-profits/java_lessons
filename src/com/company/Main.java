58d9c6e0e4ba3ae3d8e01c362f50bcbb4b92d088
profits <v-profit@yandex.ru>
2021-08-18 14:59:31 +0400

    1. объновление Circle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,24 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Time t1 = new Time();
-        System.out.println(t1);
+        Circle с1 = new Circle();
+        System.out.println(с1.toString()); // явный вызов метода toString()
+        System.out.println(с1); //неявное обращение к с1.toString()
+        System.out.println("Объект c1 это: " + с1); //'+' вызывает с1.toString(), чтобы получить строку до конкатенации
 
-        Time t2 = new Time(1,2,3);
-        for(int i = 0; i < 3; ++i)
-            System.out.println(t2.nextSecond());
-
-        t2.setHour(4);
-        t2.setMinute(5);
-        t2.setSecond(6);
-        System.out.println(t2);
-        System.out.println("Hour: " + t2.getHour());
-        System.out.println("Minute: " + t2.getMinute());
-        System.out.println("Secund: " + t2.getSecond());
-
-        t2.setTime(23,59,58);
-        System.out.println(t2);
-        System.out.println(t2.nextSecond());
-        System.out.println(t2.nextSecond().nextSecond().nextSecond());
     }
 }
