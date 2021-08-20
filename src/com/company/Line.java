3c2604cda6a9bfa90e033d9269867737ee118cec
profits <v-profit@yandex.ru>
2021-08-20 22:26:01 +0400

    2. задание 4

--- src/com/company/Line.java
+++ src/com/company/Line.java
@@ -12,35 +12,32 @@ public class Line {
         this.begin = begin;
         this.end = end;
     }
-
     public Point getBegin(){ return this.begin; }
-    public void setBegin(Point begin){ this.begin = begin; }
     public Point getEnd(){ return this.end; }
-    public void setEnd(Point end){ this.end = end; }
-
     public int getBeginX(){ return this.begin.getX(); }
-    public void setBeginX(int x){ begin.setX(x); }
     public int getEndX(){ return this.end.getX(); }
-    public void setEndX(int x){ end.setX(x); }
+    public int getBeginY(){ return this.begin.getY(); }
+    public int getEndY(){ return this.end.getY(); }
 
-    public int getBeginY(){ return this.begin.getX(); }
-    public void setBeginY(int x){ begin.setX(x); }
-    public int getEndY(){ return this.end.getX(); }
-    public void setEndY(int x){ end.setX(x); }
+    public void setBegin(Point begin){ this.begin = begin; }
+    public void setEnd(Point end){ this.end = end; }
+    public void setBeginX(int x){ begin.setX(x); }
+    public void setEndX(int x){ end.setX(x); }
+    public void setBeginY(int y){ begin.setY(y); }
+    public void setEndY(int y){ end.setY(y); }
 
     public int[] getBeginXY(){ return new int[]{ this.begin.getX(), this.begin.getY() }; }
-    public void setBeginXY(int x, int y){ begin.setX(x); begin.setY(y); }
     public int[] getEndXY(){ return this.end.getXY(); }
+    public void setBeginXY(int x, int y){ begin.setX(x); begin.setY(y); }
     public void setEndXY(int x, int y){ end.setXY(x, y); }
 
-    public String toString(){
-        //return "Line:[begin: (" + this.begin.getX() + ", " + this.begin.getY() + "), " +
-        //"end: (" + this.end.getX() + ", " + this.end.getY() + ")]";
-        return "Line: [begin: " + begin + ", " + "end: " + end + "]";
-    }
-    public double getLength(){
-        //return Math.sqrt(Math.abs(Math.pow(this.begin.getX() - this.end.getX(), 2) +
-        //       Math.pow(this.begin.getY() - this.end.getY(), 2)));
-        return begin.distance(end);
+    public String toString(){ return "Line: [begin: " + begin + ", " + "end: " + end + "]"; }
+    public double getLength(){ return begin.distance(end); }
+
+    //=== глава 2, задание 4
+    public double getGradient() {
+        int xx = Math.abs(this.getBeginX() - this.getEndX());
+        int yy = Math.abs(this.getBeginY() - this.getEndY());
+        return Math.toDegrees(1.0 * yy / xx);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        Circle c1 = new Circle();
-        System.out.println(c1.toString());
-        System.out.printf("Площадь: %.2f%n", c1.getArea());
-
-        Circle c2 = new Circle(1, 1, 3.0);
-        System.out.println(c2.toString());
-        System.out.printf("Площадь: %.2f%n", c2.getArea());
-        System.out.printf("Дистанция: %.2f%n", c2.distance(c1));
-
-        Circle c3 = new Circle(new Point(3, 4), 3.5);
-        System.out.println(c3);
-        System.out.printf("Площадь: %.2f%n", c3.getArea());
-        System.out.println("Центр: " + c3.getCenter());
-        System.out.printf("Окружность: %.2f%n", c3.getCircumference());
-        System.out.printf("Дистанция: %.2f%n", c3.distance(c1));
+        Line line = new Line(new Point(2, 4), new Point(34, 145));
+        System.out.println(line);
+        System.out.printf("Угол наклона к оси X: %.2f градусов\n", line.getGradient());
 
     }
 }
