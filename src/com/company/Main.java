dd4f8cb1ac88909adac824965ec70d2d605276ef
profits <v-profit@yandex.ru>
2021-08-25 18:40:30 +0400

    5.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,17 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        Movable m = new MovablePoint(3, 5);
+        MovablePoint m2 = (MovablePoint)m;
+        MovablePoint mp = new MovablePoint(8, 9);
 
-        //Shape_2 s = new Shape_2();
-        Shape s2 = new Rectangle("blue", 5, 5);
-        Shape s3 = new Triangle("black", 5, 5);
-        System.out.println(/*s+"\n"+*/s2+"\n"+s3);
+        System.out.println(m + "" + m2 + mp);
 
+        m.moveDown();
+        m2.moveLeft();
+        mp.moveRigth();
+        mp.moveUp();
+
+        System.out.println(m + "" + m2 + mp);
     }
 }
---
+++ src/com/company/Movable.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface Movable {
+    //===
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRigth();
+}
